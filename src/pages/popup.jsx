9d99c0203aeaa268b853998060c76efc63ee9643
snowjang24 <asdka4618@gmail.com>
2021-02-04 20:36:56 +0900

    feat: tailwind installed

--- src/pages/popup.jsx
+++ src/pages/popup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "@src/styles/tailwind.css"
 const PopupPage = () => {
   return (
     <div>
--- src/pages/window.jsx
+++ src/pages/window.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "@src/styles/tailwind.css"
 
 const WindowPage = () => {
   return (
-    <div>
-      <h1>Window Page</h1>
+    <div className='bg-black mx-5 hover:bg-white'>
+      <h1 className='text-white hover:text-red-900'>Window Page</h1>
     </div>
   );
 };
