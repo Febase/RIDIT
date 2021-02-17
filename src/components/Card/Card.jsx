146523ca9a85a98b67df4c25462646d81014e68e
Emma Moon <emma@freshcode.me>
2021-02-17 22:00:27 +0900

    init

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import "@src/styles/tailwind.css"
 
-
-const Card = ({ title, paragraph }) => (
-  <aside>
-    <h2>{title}</h2>
-    <p>{paragraph}</p>
-  </aside>
+const Card = ({ title, paragraph, url }) => (
+  <a href={url} target="_blank" className="bg-white hover:bg-gray-200 rounded-md overflow-hidden mx-auto mt-4 shadow-lg w-11/12 md:w-4/5 lg:w-3/6 divide-y text-left">
+    <h2 className="py-2 px-8">{title}</h2>
+    <div className="flex">
+      <p className="py-2 px-8">{paragraph}</p>
+    </div>
+    
+  </a>
 );
 
 export default Card;
--- src/pages/window.jsx
+++ src/pages/window.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import Card from "@src/components/Card/Card.jsx"
 import "@src/styles/tailwind.css"
 
 const WindowPage = () => {
   return (
-    <div className='bg-black mx-5 hover:bg-white'>
-      <h1 className='text-white hover:text-red-900'>Window Page</h1>
+    <div className="bg-yellow-100 h-screen">
+      <header className="text-center p-8">
+        <h1 className="text-3xl font-bold tracking-widest">RIDIT</h1>
+        <p className="">Read it 👀, Get Rid of it!</p>
+      </header>
+      <section className="text-center">
+        <h1 className="text-xl font-light pb-4">
+          ✔️ Check Reminders 
+          <span class="text-base text-white bg-gray-900 rounded-full px-2 ml-2 inline-block align-text-bottom ">1/3</span>
+        </h1>
+        <div className="flex flex-col">
+          <Card title="aaa" paragraph="bbb" url="https://www.naver.com"/>
+          <Card title="aaa" paragraph="bbb"/>
+          <Card title="aaa" paragraph="bbb"/>
+        </div>
+      </section>
+      
     </div>
   );
 };
