3eec39c74afc5c74129c713c6e2b7993d8c9a597
wooooooood <13pft13@gmail.com>
2021-03-28 18:05:38 +0900

    feat: show dropdown of bookmark folders

--- src/pages/popup.jsx
+++ src/pages/popup.jsx
@@ -4,6 +4,7 @@ import "@src/styles/tailwind.css"
 const PopupPage = () => {
   const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
+  const [bookmarkFolders, setBookmarkFolders] = useState([]);
   const titleRef = React.createRef("");
   const urlRef = React.createRef("");
 
@@ -12,6 +13,15 @@ const PopupPage = () => {
       setUrl(tabs[0].url);
       setTitle(tabs[0].title);
     });
+
+    chrome.bookmarks.getTree((tree) => {
+      setBookmarkFolders([
+        ...tree[0].children
+      ]);
+
+      console.log([...tree[0].children]);
+      console.log(bookmarkFolders);
+    })
   },[])
 
   const SaveBookMarkClicked = () => {
@@ -31,6 +41,11 @@ const PopupPage = () => {
     <div className="m-2" style={{width: "250px"}}>
       <input className="w-full border my-1 p-1" placeholder="Title" ref={titleRef} defaultValue={title} onChange={onTitleChanged}/>
       <input className="w-full border my-1 p-1" placeholder="Url" ref={urlRef} defaultValue={url} onChange={onUrlChanged}/>
+      <select className="w-full border my-1 p-1" name="folders" id="bookmark-folders">
+        {
+          bookmarkFolders.map((value) => <option value={value.title}>{value.title}</option>)
+        }
+      </select>
       <button className="w-full text-purple bg-purple-light rounded mt-2 py-1" onClick={SaveBookMarkClicked}>SAVE IT</button>
     </div>
   );
